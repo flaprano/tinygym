478b74f1d7edd535390add336fa9bf564b41c30b
Felipe Laprano <flaprano@gmail.com>
2017-07-16 15:49:01 -0300

    visitor create regular user

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    email 'felipe@laprano.com.br'
+    name 'Felipe Laprano'
+    password '123456'
+  end
+end
---
+++ spec/features/visitor_create_regular_user_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+feature 'Visitor create regular user' do
+  scenario 'successfully' do
+    #Dados
+
+    #Naveggação
+    visit root_path
+    click_on 'Create User'
+
+    fill_in 'Email', with: 'teste@gmail.com'
+    fill_in 'Name', with: 'João da Silva'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    #Expectativa
+    expect(current_path).to eq(root_path)
+    expect(page).to have_css('p', text: 'Welcome! You have signed up successfully.')
+  end
+
+  scenario 'and password < 6' do
+    #Dados
+
+    #Naveggação
+    visit root_path
+    click_on 'Create User'
+
+    fill_in 'Email', with: 'teste@gmail.com'
+    fill_in 'Name', with: 'João da Silva'
+    fill_in 'Password', with: '12345'
+    fill_in 'Password confirmation', with: '12345'
+
+    click_on 'Sign up'
+
+    #Expectativa
+    expect(page).to have_css('li', text: 'Password is too short (minimum is 6 characters)')
+  end
+
+  scenario 'and password confirmation dont match' do
+    #Dados
+
+    #Naveggação
+    visit root_path
+    click_on 'Create User'
+
+    fill_in 'Email', with: 'teste@gmail.com'
+    fill_in 'Name', with: 'João da Silva'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123455'
+
+    click_on 'Sign up'
+
+    #Expectativa
+    expect(page).to have_css('li', text: 'Password confirmation doesn\'t match Password')
+  end
+
+  scenario 'and email has already been taken' do
+    #Dados
+
+    #Naveggação
+    visit root_path
+    click_on 'Create User'
+
+    fill_in 'Email', with: 'teste@gmail.com'
+    fill_in 'Name', with: 'João da Silva'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    sign_out :user
+
+    click_on 'Create User'
+
+    fill_in 'Email', with: 'teste@gmail.com'
+    fill_in 'Name', with: 'João da Silva'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    #Expectativa
+    expect(page).to have_css('li', text: 'Email has already been taken')
+  end
+end
